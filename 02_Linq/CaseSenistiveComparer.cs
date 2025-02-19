51628c4e6ca3a14a457f497304a79b46c9cf565f
Islam <islamcobra00@gmail.com>
2025-02-19 18:37:58 +0200

    Add project files.

---
+++ 02_Linq/CaseSenistiveComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Linq
+{
+    class CompareWithNonCaseSensitive : IComparer<string>
+    {
+
+        public int Compare(string? x, string? y)
+        {
+            return string.Compare(x, y, true);
+        }
+
+    }
+}
---
+++ 02_Linq/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _02_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Restriction Operators
+            //Q1
+
+            //Q2
+
+
+            //Q3
+
+            #endregion
+            #region LINQ - Restriction Operators
+            //Q1
+
+            //Q2
+
+
+            //Q3
+
+            #endregion
+            #region LINQ - Restriction Operators
+            //Q1
+
+            //Q2
+
+
+            //Q3
+
+            #endregion
+            #region LINQ - Restriction Operators
+            //Q1
+
+            //Q2
+
+
+            //Q3
+
+            #endregion
+        }
+    }
+}
